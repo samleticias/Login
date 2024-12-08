f1aa67764482c93cd13f5efa3fd874d0aba3246a
samleticias <sammyaleticia09@gmail.com>
2024-12-08 20:45:19 -0300

    feat: add method to save user

--- src/main/java/com/example/login/service/UserServiceImpl.java
+++ src/main/java/com/example/login/service/UserServiceImpl.java
@@ -1,16 +1,47 @@
 package com.example.login.service;
 
 import com.example.login.dto.UserDTO;
+import com.example.login.entity.Role;
 import com.example.login.entity.User;
+import com.example.login.repository.RoleRepository;
+import com.example.login.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
     @Override
-    public void saveUser(UserDTO userDto) {
-        
+    public void saveUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getFirstName() + " " + userDTO.getLastName());
+        user.setEmail(userDTO.getEmail());
+        // criptografia de senha usando Spring Security
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        Role role = roleRepository.findByName("ROLE_ADMIN");
+        if (role == null) {
+            role = checkRoleExist();
+        }
+        user.setRoles(List.of(role));
+        userRepository.save(user);
+    }
+
+    private Role checkRoleExist() {
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        return roleRepository.save(role);
     }
 
     @Override
