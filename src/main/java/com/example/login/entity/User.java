77f95c33ca15a13a7ca6e4bfad3f95cad4b0c498
samleticias <sammyaleticia09@gmail.com>
2024-12-08 20:27:50 -0300

    feat: create user entity

---
+++ src/main/java/com/example/login/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.login.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
