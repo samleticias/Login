c855d391a0342bc080ad80ec7f51cad55837ae8b
samleticias <sammyaleticia09@gmail.com>
2024-12-08 21:14:32 -0300

    feat: add endpoint to redirect to registration page

--- src/main/java/com/example/login/controller/AuthController.java
+++ src/main/java/com/example/login/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.login.controller;
 
+import com.example.login.dto.UserDTO;
 import com.example.login.service.UserService;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,5 +20,14 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    // método para redirecionar para a página de registro
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        // cria objeto de modelo para armazenar dados do formulário
+        UserDTO user = new UserDTO();
+        model.addAttribute("user", user);
+        return "register";
     }
 }
